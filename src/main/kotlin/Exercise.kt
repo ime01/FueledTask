685f748b3724a79761cf8cd2d03cedd0334ba3b1
FLOWZ <ketuimevbore@yahoo.com>
2020-07-14 03:54:44 -0700

    Cloned the repo, Started Task, Created the Data Models

--- src/main/kotlin/Exercise.kt
+++ src/main/kotlin/Exercise.kt
@@ -26,7 +26,59 @@ import resources.Data
 
 // 1. First, start by modeling the data objects that will be used.
 
+data class BlogResults (val users: Array<Users>, val comments: Array<Comments>, val post: Array<Post> )
 
+
+
+data class Post (
+        val userId: Int,
+        val id: Int,
+        val title : String,
+        val body: String
+)
+
+data class Comments (
+      val postId : Int,
+      val email: String,
+      val name:  String,
+      val body:  String,
+      val id: Int
+)
+
+
+data class Users (
+        val id: Int,
+        val name: String,
+        val username: String,
+        val email: String,
+        val address : addressObject,
+        val phone: String,
+        val website: String,
+        val company: companyObject
+
+
+)
+
+data class companyObject (
+        val name: String,
+        val catchPhrase: String,
+        val bs: String
+)
+
+data class addressObject (
+        val street: String,
+        val suite: String,
+        val city: String,
+        val zipcode: Long,
+        val geo : geoObject
+
+
+)
+
+data class geoObject (
+        val lat: Double,
+        val Long: Double
+)
 
 
 
@@ -35,6 +87,13 @@ fun main(vararg args: String) {
 
     // 2. Next, decode the JSON source using `[Data.getUsers()]`
 
+    BlogResults(
+            users = Data.getUsers(),
+
+            post = Data.getPosts(),
+
+            comments = Data.getComments()
+    )
 
 
     // 3. Finally, calculate the average number of comments per user and use it
@@ -42,4 +101,8 @@ fun main(vararg args: String) {
 
 
 
+
+
 }
+
+
