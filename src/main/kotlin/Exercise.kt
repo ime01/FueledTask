4b7bbde3b0e02ec20e413170a7b1909038f74793
FLOWZ <ketuimevbore@yahoo.com>
2020-07-17 03:44:01 -0700

    looped through posts and now gotten the total post made by each user

--- src/main/kotlin/Exercise.kt
+++ src/main/kotlin/Exercise.kt
@@ -26,9 +26,14 @@ import resources.Data
 
 // 1. First, start by modeling the data objects that will be used.
 
+//creating the models
+
 data class BlogResults (val users: Array<Users>, val comments: Array<Comments>, val post: Array<Post> )
 
 
+data class BlogComments (val comments: Array<Comments> )
+data class BlogUsers (val users: Array<Users> )
+data class BlogPost (val post: Array<Post> )
 
 data class Post (
         val userId: Int,
@@ -69,7 +74,7 @@ data class addressObject (
         val street: String,
         val suite: String,
         val city: String,
-        val zipcode: Long,
+        val zipcode: String,
         val geo : geoObject
 
 
@@ -95,13 +100,137 @@ fun main(vararg args: String) {
             comments = Data.getComments()
     )
 
+    BlogComments(
+            comments = Data.getComments()
+    )
+
+    BlogUsers(
+            users = Data.getUsers()
+    )
+
+    BlogPost(
+            post= Data.getPosts()
+    )
+
+
 
     // 3. Finally, calculate the average number of comments per user and use it
     //    to find the 3 most engaging bloggers and output the result.
 
 
+//    metric = ratio of the total comments on their post to their total posts.
+
+//    the logic = get a user with their id, get their number of total comments on their post and by the number of their total post, it would give us the answer values from which we can select the top 3 Users.
 
 
+    val dataSet = BlogResults(Data.getUsers(), Data.getPosts(), Data.getComments())
+
+
+    val blogComments = BlogComments(Data.getComments())
+    val blogUsers = BlogUsers(Data.getUsers())
+    val blogPost = BlogPost(Data.getPosts())
+
+    val totalPostsSize = blogPost.post.size //100 total Posts
+
+
+    var c = blogPost.post.get(0).userId..blogPost.post.get(99).userId
+
+
+    var leanneGrahamPosts: Int = 0
+    var ervinHowellPosts: Int = 0
+    var clementineBauchPosts: Int = 0
+    var patriciaLebsackPosts: Int = 0
+    var chelseyDietrichPosts: Int = 0
+    var mrsDennisSchulistPosts: Int = 0
+    var kurtisWeissnatPosts: Int = 0
+    var nicholasRunolfsdottirVPosts: Int = 0
+    var glennaReichertPosts: Int = 0
+    var clementinaDuBuquePosts: Int = 0
+
+
+//Moving through Posts and fetching how many Total Post each User Made
+
+
+    for (i in blogPost.post.indices){
+
+//        println(blogPost.post[i].userId)
+        if (blogPost.post[i].userId==1){
+
+            leanneGrahamPosts++
+        }
+
+        if (blogPost.post[i].userId==2){
+
+            ervinHowellPosts++
+        }
+
+        if (blogPost.post[i].userId==3){
+
+            clementineBauchPosts++
+        }
+
+        if (blogPost.post[i].userId==4){
+
+            patriciaLebsackPosts++
+        }
+
+        if (blogPost.post[i].userId==5){
+
+            chelseyDietrichPosts++
+        }
+
+        if (blogPost.post[i].userId==6){
+
+            mrsDennisSchulistPosts++
+        }
+
+        if (blogPost.post[i].userId==7){
+
+            kurtisWeissnatPosts++
+        }
+
+        if (blogPost.post[i].userId==8){
+
+            nicholasRunolfsdottirVPosts++
+        }
+
+        if (blogPost.post[i].userId==9){
+
+            glennaReichertPosts++
+        }
+
+        if (blogPost.post[i].userId==10){
+
+            clementinaDuBuquePosts++
+        }
+
+
+    }
+    //    println(chelseyDietrichPosts)
+//    from this it is noted that each user had made a 10 posts
+
+
+//get total number of comments
+
+
+    val totalComments = blogComments.comments.size
+
+//    println(totalComments)
+    //total of 500 comments made by guests
+
+//    Now moving through comments using the PostId field to determine who made the post
+
+    var commentsAboutLeanneGrahamPosts: Int = 0
+
+
+
+    for (i in blogComments.comments.indices){
+
+        if (blogComments.comments[i].postId == 1){
+
+        }
+
+    }
 
 }
 
